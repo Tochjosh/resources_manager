65121ee28522f5f061519ba16f0475ccaa95fc52
Tochjosh <tochjosh@gmail.com>
2021-07-16 20:06:37 +0100

    Using decorators to protect company's resources.

--- email_parser.py
+++ email_parser.py
@@ -1,4 +1,24 @@
+import re
+
 
 def email_parser(email):
-  pass
+    # pattern is generated and compiled below
+    email_pattern = re.compile(r'[a-zA-Z][a-zA-Z0-9+]*[a-zA-Z0-9]+@[a-zA-Z][a-zA-Z0-9]+(\.com)$')
+    check_email = re.match(email_pattern, email)  # the email is matched against the pattern here
+
+    # check to make sure email entered is of a string, else return None
+    if not type(email) is str:
+        return None
+
+    if check_email:
+        group_email = check_email.group()  # email is grouped here
+        split_email = re.split("@", group_email)  # the grouped email is split at the @ here to return a list
+        # the list returned is iterated and used to generate a dictionary below.
+        record = {"Username": split_email[0],
+                  "Domain": split_email[1]
+                  }
+        return record
+    else:
+        return check_email
+
 
--- secure_resource/resource_deco.py
+++ secure_resource/resource_deco.py
@@ -1,40 +1,42 @@
 from datetime import datetime
 from auth import authenticate
-from contextlib import contextmanager
 
 
 def resource_deco(email, password):
-    def middle(resource):
+    def middle(resource):  # if decorator takes more than one parameter, then an intermediate func. is used.
         def deco_wrapper():
-            authenticated = authenticate(email, password)
-            if authenticated:
-                staff_name = authenticated['first_name'] + " " + authenticated['last_name']
-                staff_role = authenticated['role']
-                time = datetime.now()
+            # Authentication of the user is verified below,
+            # using the authentication function that loops through the database of staffs
+            authenticated_staff = authenticate(email, password)
+
+            if authenticated_staff:
+                # if the user passes for a staff, the details are extracted below
+                staff_name = authenticated_staff['first_name'] + " " + authenticated_staff['last_name']
+                staff_role = authenticated_staff['role']
+                time = datetime.now()  # datetime module is imported and used here to get the present time
+
+                # datetime is scrapped for time and date below
                 date_display = time.strftime("%x")
                 time_display = time.strftime("%H:%M")
 
-                if authenticated['role'] == 'admin' or authenticated['role'] == 'superadmin':
-                    allowed = resource()
+                if authenticated_staff['role'] == 'admin' or authenticated_staff['role'] == 'superadmin':
+                    # the admin and superadmin is allowed here to access the company's resource.
+                    company_resource = resource()
 
-                    @contextmanager
-                    def open_file(file, mode):
-                        logger_list = open('access_granted.txt', 'a')
-                        yield logger_list
-                        logger_list.close()
-
-                    with open_file("access_granted.txt", "a") as staff_log:
+                    with open("access_granted.txt", "a") as staff_log:
                         staff_log.write(
-                            f"{staff_role} {staff_name} viewed company resources on {date_display} at {time_display}")
-                    return allowed
+                            f"\n{staff_role} {staff_name} viewed company resources on {date_display} at {time_display}")
+                    return company_resource  # company resource is made available here
 
                 else:
+                    # staffs below admin and superadmin level are denied access here, and their details logged here.
                     with open("access_denied.txt", "a") as staff_log:
                         staff_log.write(
-                            f"{staff_role} {staff_name} viewed company resources on {date_display} at {time_display}")
+                            f"\n{staff_role} {staff_name} viewed company resources on {date_display} at {time_display}")
                     return "You are not allowed to view this resource"
+                    # an access denied message is returned to the staff here
 
-            else:
+            else:  # anonymous person is denied access here
                 return "Only staffs can access this resource"
 
         return deco_wrapper
